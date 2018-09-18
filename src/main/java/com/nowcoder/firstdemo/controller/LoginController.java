71f9f1d22815ec6af091c73c1b62969c2bbf9172
ae <1141239384@qq.com>
2018-09-18 19:54:56 +0800

    第二次提交

--- src/main/java/com/nowcoder/firstdemo/controller/LoginController.java
+++ src/main/java/com/nowcoder/firstdemo/controller/LoginController.java
@@ -6,10 +6,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 /**
@@ -26,15 +29,19 @@ public class LoginController {
     @RequestMapping(path = {"/reg/"}, method = {RequestMethod.POST})
     public String reg(Model model,
                       @RequestParam("username") String username,
-                      @RequestParam("password") String password) {
+                      @RequestParam("password") String password,
+                      HttpServletResponse response) {
         try {
             Map<String, String> map = userService.register(username, password);
-            if (map.containsKey("msg")) {
+            if (map.containsKey("ticket")) {
+                Cookie cookie = new Cookie("ticket", map.get("ticket"));
+                cookie.setPath("/");
+                response.addCookie(cookie);
+                return "redirect:/";
+            } else {
                 model.addAttribute("msg", map.get("msg"));
                 return "login";
             }
-
-            return "redirect:/";
         }catch (Exception e) {
             logger.error("注册异常" + e.getMessage());
             return "login";
@@ -44,5 +51,34 @@ public class LoginController {
     @RequestMapping(path = {"/regLogin"}, method = {RequestMethod.GET})
     public String reg(Model model) {
        return "login";
+    }
+
+    @RequestMapping(path = {"/login/"}, method = {RequestMethod.POST})
+    public String login(Model model,
+                        @RequestParam("username") String username,
+                        @RequestParam("password") String password,
+                        @RequestParam(value = "rememberme", defaultValue = "false") boolean rememberme,
+                        HttpServletResponse response) {
+        try {
+            Map<String, String> map = userService.login(username, password);
+            if (map.containsKey("ticket")) {
+                Cookie cookie = new Cookie("ticket", map.get("ticket"));
+                cookie.setPath("/");
+                response.addCookie(cookie);
+                return "redirect:/";
+            } else {
+                model.addAttribute("msg", map.get("msg"));
+                return "login";
+            }
+        }catch (Exception e) {
+            logger.error("登录异常" + e.getMessage());
+            return "login";
+        }
+    }
+
+    @RequestMapping(path = {"/logout"}, method = {RequestMethod.GET})
+    public String logout(@CookieValue("ticket") String ticket) {
+        userService.logout(ticket);
+        return "redirect:/";
     }
 }
--- src/main/java/com/nowcoder/firstdemo/service/UserService.java
+++ src/main/java/com/nowcoder/firstdemo/service/UserService.java
@@ -1,6 +1,8 @@
 package com.nowcoder.firstdemo.service;
 
+import com.nowcoder.firstdemo.dao.LoginTicketDAO;
 import com.nowcoder.firstdemo.dao.UserDAO;
+import com.nowcoder.firstdemo.model.LoginTicket;
 import com.nowcoder.firstdemo.model.User;
 import com.nowcoder.firstdemo.util.WendaUtil;
 import org.apache.commons.lang.StringUtils;
@@ -8,10 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * Created by ae on 2018/8/11.
@@ -20,7 +19,10 @@ import java.util.UUID;
 @Service
 public class UserService {
     @Autowired
-    UserDAO userDAO;
+    private UserDAO userDAO;
+
+    @Autowired
+    private LoginTicketDAO loginTicketDAO;
 
     public Map<String, String> register(String username, String password) {
         Map<String, String> map = new HashMap<String, String>();
@@ -46,13 +48,62 @@ public class UserService {
         user.setSalt(UUID.randomUUID().toString().substring(0,5));
         user.setHeadUrl(String.format("http://images.nowcoder.com/head/%dt.png", new Random().nextInt(1000)));
         user.setPassword(WendaUtil.MD5(password + user.getSalt()));
-
         userDAO.addUser(user);
+
+        String ticket = addLoginTicket(user.getId());
+        map.put("ticket", ticket);
 
         return map;
     }
 
+
     public User getUser(int id) {
         return userDAO.selectById(id);
+    }
+
+
+    public Map<String, String> login(String username, String password) {
+        Map<String, String> map = new HashMap<String, String>();
+
+        if (StringUtils.isBlank(username)) {
+            map.put("msg", "用户名不能为空");
+            return map;
+        }
+
+        if (StringUtils.isBlank(password)) {
+            map.put("msg", "密码不能为空");
+            return map;
+        }
+
+        User user = userDAO.selectByName(username);
+        if (user == null) {
+            map.put("msg", "用户名不存在");
+            return map;
+        }
+
+        if (!WendaUtil.MD5(password + user.getSalt()).equals(user.getPassword())) {
+            map.put("msg", "密码错误");
+            return map;
+        }
+
+        String ticket = addLoginTicket(user.getId());
+        map.put("ticket", ticket);
+        return map;
+    }
+
+    public String addLoginTicket(int userId) {
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setUserId(userId);
+        Date now = new Date();
+        now.setTime(3600 * 24 * 100 + now.getTime());
+        loginTicket.setExpired(now);
+        loginTicket.setStatus(0);
+        loginTicket.setTicket(UUID.randomUUID().toString().replaceAll("-",""));
+        loginTicketDAO.addTicket(loginTicket);
+        return loginTicket.getTicket();
+    }
+
+    public void logout(String ticket) {
+        loginTicketDAO.updateStatus(ticket, 1);
     }
 }
